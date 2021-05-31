36e3f184ec5dc55aa5828772c0b581a5b2572cf7
Prakhar Singh <singhprakhar973@gmail.com>
2021-05-31 10:58:51 +0530

    Update _dbconnect.php

--- login page/_dbconnect.php
+++ login page/_dbconnect.php
@@ -1,8 +1,11 @@
 <?php
-$server = "localhost";
-$username = "root";
-$password = "";
-$database = "users";
+
+/*mysql://bef5368dd5de39:44d1b1e9@us-cdbr-east-03.cleardb.com/heroku_4e5e8e7a134ee78?reconnect=true */
+
+$server = "us-cdbr-east-03.cleardb.com";
+$username = "bef5368dd5de39";
+$password = "44d1b1e9";
+$database = "heroku_4e5e8e7a134ee78";
 
 $conn = mysqli_connect($server, $username, $password, $database);
 if(!$conn){
